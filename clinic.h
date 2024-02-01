c1227ea212763d6bb61ac374b6ce1c03d56218f1
Mustafa Dohad <123301934+mustafa-dohad@users.noreply.github.com>
2024-01-31 20:58:32 -0500

    Update clinic.h

--- clinic.h
+++ clinic.h
@@ -1,16 +1,3 @@
-///////////////////////////////////////////////////////////////////////////
-// Assignment 1 - Milestone 3
-// Full Name  : Mustafa Dohad
-// Student ID#: 123090235
-// Email      : mna26@myseneca.ca
-// Section    : ZGG
-//
-// Authenticity Declaration:
-// I declare this submission is the result of my own work and has not been
-// shared with any other student or 3rd party content provider. This submitted
-// piece of work is entirely of my own creation.
-///////////////////////////////////////////////////////////////////////////
-
 // SAFE-GUARD:
 // It is good practice to apply safe-guards to header files
 // Safe-guard's ensures only 1 copy of the header file is used in the project build
